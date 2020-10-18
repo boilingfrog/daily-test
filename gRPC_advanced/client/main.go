ca00544d3c269465ed299035a0d6a33ff9cbb000
rick.lizhan@gmail.com <rick.lizhan@gmail.com>
2020-10-18 20:24:50 +0800

    gRPC

---
+++ gRPC_advanced/client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"daily-test/gRPC_advanced"
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc/credentials"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	creds, err := credentials.NewClientTLSFromFile(
+		"/Users/yj/goWork/daily-test/gRPC_advanced/server.crt", "main.HelloService",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := grpc.Dial("localhost:1234",
+		grpc.WithTransportCredentials(creds),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := gRPC_advanced.NewHelloServiceClient(conn)
+	reply, err := client.Hello(context.Background(), &gRPC_advanced.String{Value: "hello"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(reply.GetValue())
+
+	fmt.Println(grpc.ServiceInfo{})
+}
---
+++ gRPC_advanced/server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"daily-test/gRPC_advanced"
+	"log"
+	"net"
+
+	"google.golang.org/grpc/credentials"
+
+	"google.golang.org/grpc"
+)
+
+type HelloServiceImpl struct{}
+
+func (p *HelloServiceImpl) Hello(
+	ctx context.Context, args *gRPC_advanced.String,
+) (*gRPC_advanced.String, error) {
+	reply := &gRPC_advanced.String{Value: "hello:" + args.GetValue()}
+	return reply, nil
+}
+
+func main() {
+
+	creds, err := credentials.NewServerTLSFromFile("/Users/yj/goWork/daily-test/gRPC_advanced/server.crt", "/Users/yj/goWork/daily-test/gRPC_advanced/server.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
+	gRPC_advanced.RegisterHelloServiceServer(grpcServer, new(HelloServiceImpl))
+
+	lis, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal(err)
+	}
+	grpcServer.Serve(lis)
+}
