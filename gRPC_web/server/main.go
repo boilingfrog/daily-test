5f85bb00c34323bf592b41bf5dfe8c1dee5392e1
rick.lizhan@gmail.com <rick.lizhan@gmail.com>
2020-10-20 22:33:14 +0800

    gRPC拦截器

---
+++ gRPC_web/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"daily-test/gRPC_web"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type HelloServiceImpl struct{}
+
+func (p *HelloServiceImpl) Hello(
+	ctx context.Context, args *gRPC_web.String,
+) (*gRPC_web.String, error) {
+	reply := &gRPC_web.String{Value: "hello:" + args.GetValue()}
+	return reply, nil
+}
+
+func main() {
+
+	grpcServer := grpc.NewServer()
+	gRPC_web.RegisterHelloServiceServer(grpcServer, new(HelloServiceImpl))
+
+	lis, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal(err)
+	}
+	grpcServer.Serve(lis)
+}
