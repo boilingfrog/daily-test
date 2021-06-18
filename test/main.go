3339ddfc3b567db0c7ead4ec6fe66ca551035d41
liz <rick.lizhan@gmail.com>
2021-06-18 21:26:57 +0800

    test

--- test/main.go
+++ test/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import "golang.org/x/sync/errgroup"
+
+type RecentlyHandleQuestionnaireBookOutput struct {
+	RecommendBooks []string `json:"recommendBooks,omitempty"`
+	UserTagDesc    string   `json:"userTagDesc,omitempty"`
+	LikePercent    float64  `json:"likePercent,omitempty"` // 用户喜欢的百分比
+	IsFirst        bool     `json:"isFirst"`
+}
 
 func main() {
-	fmt.Println(1 % 10)
+	var output = &RecentlyHandleQuestionnaireBookOutput{}
+
+	var eg errgroup.Group
+
+	eg.Go(func() error {
+		output.IsFirst = true
+		return nil
+	})
+
+	eg.Go(func() error {
+		output.LikePercent = 32
+		return nil
+	})
+
+	if err := eg.Wait(); err != nil {
+		return
+	}
+
 }
